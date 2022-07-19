d9e311b46e9ee7d780a2ab4cf2f3e8c04fb5e408
jamesmacallan <108479492+jamesmacallan@users.noreply.github.com>
2022-07-19 09:50:32 -0500

    Add files via upload

--- vcsuccess.R
+++ vcsuccess.R
@@ -1,21 +1,33 @@
+library(dplyr)
+
 get(wd) #gets working directory
-vcsuccess <- readr::read_csv("C:/Users/jmand/OneDrive/Documents/CSVs/vcsuccess.csv") #reads csv, my csv only has two columns
-#I plan to import a different csv with the other usable data.
+vcsuccess <- readr::read_csv("C:filepath")
 spec(vcsuccess) #specifies how r is viewing the csv, i used this to make sure it was reading the correct columns
 class(vcsuccess) #show if it can be seen as dataframe i think
 
 df1<-data.frame(vcsuccess) #creates dataframe
-complete.cases(df1) #gives boolean for complete x and y columns
-na_vec <- which(!complete.cases(df1)) #specifies non complete columns (notice the exclamation point)
-df1 <- df1[-na_vec,] #removes anything with na
 #print(df1)
-df1$status<-ifelse(df1$status=='operating'||df1$status=='acquired',1,0) #creates value for qualitative data
+#df1$status <- if(df1$status=='acquired', "-")
+#df1 <- subset(df1,status != "acquired")#hyphonates any status that's acquired to get rid of later
+#complete.cases(df1) #gives boolean for complete x and y columns
+#na_vec <- which(!complete.cases(df1)) #specifies non complete columns (notice the exclamation point)
+#df1 <- df1[-na_vec,] #removes anything with na
+#print(df1)
+df1$status <- ifelse(df1$status=='operating',1,0) #creates value for qualitative data ||df1$status=='acquired',
 df1$funding_total_usd<-gsub(",","",as.character(df1$funding_total_usd)) #gets rid of commas in the funding data
 df1 <- subset(df1, funding_total_usd != "-") #gets rid of hyphon data in total funding
 print(df1)
+sum(with(df1,status == 1))
+sum(with(df1,status == 0))
 
-plot(x = df1$funding_total_usd,
-     y = df1$status,
-     xlab = "Funding",
-     ylab = "Status",
-     main = "Funding vs Status")
+# truncated_status <- function(df1, status_to_change, n) {
+#   df1 %>%
+#     filter(status %in% status_to_change) %>%
+#     group_by(status) %>%
+#     #slice_sample(n = n) %>%
+#     ungroup %>%
+#     bind_rows(df1 %>% filter(!status %in% status_to_change))
+# }
+# 
+# new_df <- df1 %>% truncated_status('operating',4000)
+# new_df %>% count(status)
